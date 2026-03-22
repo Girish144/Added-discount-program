bda295eae3c7ea6aff3261d243fb6b879ed29b30
Girish Kumbar <girishkumbar100@gmail.com>
2026-03-22 21:13:21 +0530

    Update day1.py

--- day1.py
+++ day1.py
@@ -2,5 +2,5 @@ input=1200
 if input < 0:
     print("invalid input can not be negative")
 else:
-    final=int(input*0.9)
-    print("final price is",final)
+    fn=int(input*0.9)
+    print("final price is",fn)
